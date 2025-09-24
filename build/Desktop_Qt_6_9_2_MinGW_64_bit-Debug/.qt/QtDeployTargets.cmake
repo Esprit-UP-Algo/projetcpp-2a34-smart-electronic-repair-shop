7611eb13b906d851933a6d201577a17f68a76444
boubakerbakouch <boubakerbakouch2020@gmail.com>
2025-09-24 23:11:43 +0100

    first commit

---
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_gestion-empolye_FILE C:/Users/boubaker/Documents/gestion-empolye/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/gestion-empolye.exe)
+set(__QT_DEPLOY_TARGET_gestion-empolye_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_gestion-empolye_RUNTIME_DLLS C:/Qt6.7.3/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt6.7.3/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt6.7.3/6.9.2/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "C:/Qt6.7.3/MaintenanceTool.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/boubaker/Documents/gestion-empolye/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt6.7.3/6.9.2/mingw_64" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt6.7.3/6.9.2/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Qt6.7.3/Tools/mingw1310_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Qt6.7.3/Tools/mingw1310_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
